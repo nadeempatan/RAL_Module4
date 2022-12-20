269153db195b72f93d4469b4cd19e45afea115bb
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:15:39 +0530

    first commit

---
+++ src/env/ral_env.sv
@@ -0,0 +1,38 @@
+
+// Register environment class puts together the model,adapter and predictor
+class create ral_env  extended from uvm_env
+ `uvm_component_utils (reg_env)
+ //function new
+
+
+ //register model handle
+ //adapter 
+ //predictor handle
+ 
+ //buildphase
+
+ //create instance for model adapter and predictor
+ //
+ //
+ 
+ //build model
+ //
+ 
+ //lock model
+ //
+ /
+
+
+  //connect phase
+
+//connect predictor to map
+//
+
+//connect predictor to adapter
+//
+
+// connect predictor to mointor analysis port
+
+
+endclass
+/////////
