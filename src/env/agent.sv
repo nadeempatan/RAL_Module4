55f4a295968abce11f23bea51054aa190d28342d
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:08:37 +0530

    first commit

---
+++ src/env/agent.sv
@@ -0,0 +1,17 @@
+// The agent puts together the driver, sequencer and monitor 
+class //create agent class extended from uvm_agent;
+ // registration 
+ //
+ 
+// handle of driver sequencer and monitor
+
+ //function new
+ //
+
+ //build phase
+
+
+ // create an instance for driver ,sequencer and moitor
+
+ //connect phase
+endclass
