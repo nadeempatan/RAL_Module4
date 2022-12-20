b5415b0608ed83e31b0f6a200211993a9b79c764
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:13:06 +0530

    first commit

---
+++ src/env/monitor.sv
@@ -0,0 +1,24 @@
+
+// Monitors the APB interface for any activity and reports out
+// through an analysis port
+class create monitor clas extended from uvm_monitor
+//factroy registration
+//
+
+//declare analysis port and virtual interface
+
+//function new
+//
+//
+//
+//
+
+//run phase
+  virtual task run_phase (uvm_phase phase);
+
+
+
+
+
+  endtask
+endclass
