c1e96ceb1e0651a907ae0e43cca7f0d80de7b33a
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:06:40 +0530

    first commit

---
+++ src/env/test.sv
@@ -0,0 +1,20 @@
+
+class //create testclass  extended from uvm_test
+ // factroy registration 
+ 
+
+//env handle 
+//
+//
+//
+
+//function new
+
+
+//build phase
+// Build the testbench environment 
+
+
+ //run phase
+ //start seq
+endclass
