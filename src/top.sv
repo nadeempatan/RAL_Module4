f059e870400c6ee985a805c7ed7e34156334728c
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:42:35 +0530

    first commit

--- src/top.sv
+++ src/top.sv
@@ -1,1 +1,26 @@
+//`include "uvm_macros.svh"
+//import uvm_pkg::*;
 
+`include "interface.sv"
+`include "testbench_pkg.svh"
+
+module tb_top;
+ //clock and reset genration 
+  
+   
+
+ 
+ // interface and dut instatansiation
+  
+  initial begin
+    // set interface in config_db
+    //
+       // Dump waves
+  
+  end
+  initial begin
+    run_test("test_name");
+    //#100;
+    //$finish;
+  end
+endmodule
