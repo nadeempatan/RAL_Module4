2339ded0c0972e3deb8103a18994dcb2ea0a5230
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:04:48 +0530

    first commit

--- src/env/env.sv
+++ src/env/env.sv
@@ -1,1 +1,30 @@
+class//create env class extended from uvm_env class
 
+
+//factory registration
+ 
+
+ // agent handle
+ // reg env handle
+ //
+ 
+ //function new
+
+
+// build phase 
+
+ //cretae an instance of agent and reg env
+ //
+ //
+ //
+ 
+ 
+
+//connect phase
+ // Connect analysis port of monitor with predictor, assign agent to register env
+ // and set default map of the register env
+//inside connect function
+ //connect analysis port of monitor with predictor
+ //set default map of the reg_env
+
+endclass
