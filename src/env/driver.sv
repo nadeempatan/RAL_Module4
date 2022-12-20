b5d6a9f35d831f18879cd1ad77563996ee1e7321
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:11:46 +0530

    first commit

---
+++ src/env/driver.sv
@@ -0,0 +1,28 @@
+// Drives a given apb transaction packet to the APB interface
+class create driver clas extended from uvm driver
+ // factory registration
+ 
+
+ //tarnsaction and virtual interface handle
+ //
+ //
+ 
+ //function new
+ //
+ //
+ //
+ //
+ 
+ //build pahse
+ //
+ //
+ //
+ //
+ 
+ //run pahse
+   virtual task run_phase (uvm_phase phase);
+
+
+
+ endtask
+endclass
