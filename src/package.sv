484b9103c483c49f4e1691f83973aa70d4535b5d
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-12-20 22:44:29 +0530

    first commit

---
+++ src/package.sv
@@ -0,0 +1,18 @@
+//include all the files inside pacakage 
+//
+//
+//
+
+/*`include "transaction.sv"
+`include "sequencer.sv"
+`include "monitor.sv"
+`include "driver.sv"
+`include "agent.sv"
+`include "base_seq.sv"
+`include "reg_model.sv"
+`include "reg2apb_adaptor.sv"
+`include "ral_env.sv"
+`include "env.sv"
+`include "base_test.sv"
+
+*/
