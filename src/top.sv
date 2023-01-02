f233e52b1cbe8926f1226df1b0c9c640ed92e23d
nadeempatan <114145269+nadeempatan@users.noreply.github.com>
2023-01-02 13:54:06 +0530

    updated top

--- src/top.sv
+++ src/top.sv
@@ -4,7 +4,7 @@
 `include "interface.sv"
 `include "testbench_pkg.svh"
 
-module tb_top;
+//module tb_top;
  //clock and reset genration 
   
    
@@ -12,15 +12,51 @@ module tb_top;
  
  // interface and dut instatansiation
   
-  initial begin
+ // initial begin
     // set interface in config_db
     //
        // Dump waves
   
-  end
-  initial begin
+ // end
+ //// initial begin
     run_test("test_name");
     //#100;
     //$finish;
-  end
+ // end
+//endmodule
+
+module tb;
+bit pclk;
+bit presetn;
+always #10 pclk=~pclk;
+
+initial begin
+presetn=0;
+#5 presetn=1;
+end
+
+apb_intf intf(pclk,presetn);
+
+apb_slave dut(
+.pclk(intf.pclk),
+.presetn(intf.presetn),
+.paddr(intf.paddr),
+.pwdata(intf.pwdata),
+.prdata(intf.prdata),
+.pwrite(intf.pwrite),
+.pselx(intf.pselx),
+.penable(intf.penable),
+.pready(intf.pready),
+.pslverr(intf.pslverr)
+);
+
+initial begin
+     uvm_config_db#(virtual apb_intf)::set(uvm_root::get(),"*","vif",intf);
+end
+
+initial begin
+	run_test("test");
+end
+
 endmodule
+
