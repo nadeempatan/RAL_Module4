65828d43a1fd1a8d7b4c3ff82e873c5784ab62b5
nadeempatan <114145269+nadeempatan@users.noreply.github.com>
2023-01-02 12:49:51 +0530

    updated the interface code

--- src/env/interface.sv
+++ src/env/interface.sv
@@ -1,5 +1,45 @@
 
-interface bus_if (input pclk, input presetn);
+//interface bus_if (input pclk, input presetn);
 	//declare interface signals
   
+//endinterface
+
+interface apb_intf(input logic pclk,presetn);
+
+logic [31:0] paddr;
+logic pselx;
+logic penable;
+logic pwrite;
+logic [31:0] pwdata;
+logic pready;
+logic [31:0] prdata;
+logic pslverr;
+
+clocking driver_cb @(posedge pclk);
+default input #10 output #0;
+output paddr;
+output pselx;
+output penable;
+output pwrite;
+output pwdata;
+input  pready;
+input  prdata;
+input  pslverr;
+endclocking 
+
+clocking monitor_cb @(posedge pclk);
+default input #10 output #0;
+input  paddr;
+input  pselx;
+input  penable;
+input  pwrite;
+input  pwdata;
+input  pready;
+input  prdata;
+input  pslverr;
+endclocking 
+
+modport driver_mp (output paddr, output pselx, output penable, output pwrite, output pwdata, input  pready, input  prdata, input  pslave_error, input pclk,presetn);
+modport monitor_mp(input  paddr, input  pselx, input  penable, input  pwrite, input  pwdata, input  pready, input  prdata, input  pslave_error, input pclk,presetn); 
+
 endinterface
